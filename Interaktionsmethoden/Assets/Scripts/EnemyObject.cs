13136e805f268e85dbfe4b0e5ffc3f4d9d2d5012
JanEberle <eberle.jan1997@gmail.com>
2018-12-29 14:14:43 +0100

    Projekt Teil 1
    Bewegter Boden
    Enemy spawn (defekt)
    Bewegung des Spielers
    Springen durch pusten (noch defekt)

---
+++ Interaktionsmethoden/Assets/Scripts/EnemyObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyObject : MonoBehaviour {
+
+    Transform enemytransform;
+
+    [SerializeField]
+    private float speed;
+
+    // Use this for initialization
+    void Start () {
+		enemytransform = gameObject.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if(-22 >= enemytransform.position.z)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            enemytransform.position += new Vector3(0, 0, -speed);
+        }
+	}
+}
